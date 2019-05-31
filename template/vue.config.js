05408c553ddd7ceb63faf166fdd654acf32a7b33
pumelotea <535553297@qq.com>
2019-05-31 16:55:35 +0800

    添加编译后自动打包成zip

--- template/vue.config.js
+++ template/vue.config.js
@@ -1,15 +1,39 @@
 require("./build.info.js")
 const FileManagerPlugin = require('filemanager-webpack-plugin')
-let distName = 'dist'
+let plugins=[]
+
+
 if (process.env.NODE_ENV === 'test' ){
-  distName = distName + '-test'
+  let distName = 'dist-test'
+  let disPath = './'+distName
+  let zipPath ='./'+distName+'.zip'
+  plugins.push(new FileManagerPlugin({  //初始化 filemanager-webpack-plugin 插件实例
+    onEnd: {
+      delete: [   //首先需要删除项目根目录下的dist.zip
+        zipPath
+      ],
+      archive: [ //然后我们选择dist文件夹将之打包成dist.zip并放在根目录
+        {source: disPath, destination: zipPath},
+      ]
+    }
+  }))
 }
 
 if (process.env.NODE_ENV === 'production' ){
-
+  let distName = 'dist'
+  let disPath = './'+distName
+  let zipPath ='./'+distName+'.zip'
+  plugins.push(new FileManagerPlugin({  //初始化 filemanager-webpack-plugin 插件实例
+    onEnd: {
+      delete: [   //首先需要删除项目根目录下的dist.zip
+        zipPath
+      ],
+      archive: [ //然后我们选择dist文件夹将之打包成dist.zip并放在根目录
+        {source: disPath, destination: zipPath},
+      ]
+    }
+  }))
 }
-let disPath = './'+distName
-let zipPath ='./'+distName+'.zip'
 module.exports = {
   lintOnSave: false,
   productionSourceMap: process.env.NODE_ENV === 'development',
@@ -23,17 +47,6 @@ module.exports = {
   },
   parallel: require('os').cpus().length > 1,
   configureWebpack: {  //webpack的相关配置在这里
-    plugins: [
-      new FileManagerPlugin({  //初始化 filemanager-webpack-plugin 插件实例
-        onEnd: {
-          delete: [   //首先需要删除项目根目录下的dist.zip
-            zipPath
-          ],
-          archive: [ //然后我们选择dist文件夹将之打包成dist.zip并放在根目录
-            {source: disPath, destination: zipPath},
-          ]
-        }
-      })
-    ]
+    plugins: plugins
   }
 }
