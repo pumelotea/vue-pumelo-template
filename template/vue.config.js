1e4dc4c812473934cff2bcd8a4f82b3b83ae4330
pumelotea <535553297@qq.com>
2019-05-31 16:36:13 +0800

    添加编译后自动打包成zip

--- template/vue.config.js
+++ template/vue.config.js
@@ -1,4 +1,15 @@
 require("./build.info.js")
+const FileManagerPlugin = require('filemanager-webpack-plugin')
+let distName = 'dist'
+if (process.env.NODE_ENV === 'test' ){
+  distName = distName + '-test'
+}
+
+if (process.env.NODE_ENV === 'production' ){
+
+}
+let disPath = './'+distName
+let zipPath ='./'+distName+'.zip'
 module.exports = {
   lintOnSave: false,
   productionSourceMap: process.env.NODE_ENV === 'development',
@@ -11,8 +22,18 @@ module.exports = {
     disableHostCheck: true,
   },
   parallel: require('os').cpus().length > 1,
-  configureWebpack: (config) => {
-    // 简单/基础配置，比如引入一个新插件
-
-  },
+  configureWebpack: {  //webpack的相关配置在这里
+    plugins: [
+      new FileManagerPlugin({  //初始化 filemanager-webpack-plugin 插件实例
+        onEnd: {
+          delete: [   //首先需要删除项目根目录下的dist.zip
+            zipPath
+          ],
+          archive: [ //然后我们选择dist文件夹将之打包成dist.zip并放在根目录
+            {source: disPath, destination: zipPath},
+          ]
+        }
+      })
+    ]
+  }
 }
